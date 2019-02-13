55a416a1799165b22275a28fdebb31f37a214434
Majid Rouhi <mrouhi13@gmail.com>
2019-02-13 23:03:47 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
@@ -8,20 +9,20 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-djecrety',
-    version='0.1',
+    name='djecrety',
+    version='1.0',
     packages=find_packages(),
     include_package_data=True,
     license='GNU Version 3 License',
-    description='Djecrety is a simple Django app to work with "settings.py" file.',
+    description='Djecrety package provides a command to generate a new secret key for your project and save it to settings.py file.',
     long_description=README,
-    url='https://www.djecrety.ir/',
+    url='https:/djecrety.ir/',
     author='Majid Rouhi',
     author_email='mrouhi13@gmail.com',
     classifiers=[
         'Environment :: Command Line Environment',
         'Framework :: Django',
-        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.1.5',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Version 3 License',
         'Operating System :: OS Independent',
