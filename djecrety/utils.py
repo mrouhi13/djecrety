b06be79d6820623b9ee7568be3bde0694bbbae02
Majid Rouhi <mrouhi13@gmail.com>
2019-08-23 12:53:57 +0430

    Improve fanctions and minor code imporvements

--- djecrety/utils.py
+++ djecrety/utils.py
@@ -9,52 +9,44 @@ from django.utils.crypto import get_random_string
 
 def generate_secret_key(length=50):
     """
-        Return a 50 character random string usable as a SECRET_KEY setting value.
+    Return a 50 character random string
+    usable as a `SECRET_KEY` setting value.
     """
-    try:
-        length = int(length)
-    except ValueError:
-        length = 50
-    except TypeError:
-        length = 50
-
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-
+    if not isinstance(length, int):
+        raise TypeError(
+            f'invalid literal for int() with base 10: {length}')
     return get_random_string(length, chars)
 
 
 def save_to_settings(value, parameter_name, settings_dir_name=''):
     """
-        Save the value to the given parameter in settings.py file.
+    Save the value to the given parameter in `settings.py` file.
     """
-    settings_dir = os.path.join(settings.BASE_DIR, settings_dir_name, 'settings.py')
+    if not settings_dir_name:
+        settings_dir_name = ''
 
+    settings_dir = os.path.join(settings.BASE_DIR, settings_dir_name,
+                                'settings.py')
     if not os.path.exists(settings_dir):
-        return False, f'Not found: {settings_dir}\n\nCan\'t find \'settings.py\' file,' \
-            ' please specify settings file directory name using -p argument.'
-
-    result, message = _replace_line(value, parameter_name, settings_dir)
-
-    return result, message
+        raise FileNotFoundError("Can't find `settings.py` file: "
+                                f"{settings_dir}")
+    return _replace_line(value, parameter_name, settings_dir)
 
 
 def _replace_line(value, parameter_name, settings_file):
     parameter_is_exist = False
+    if parameter_name:
+        new_line = f'{parameter_name} = {value}'
+        line_pattern = fr'^{parameter_name} = .*'
 
-    if not parameter_name:
-        return False, f'Can\'t find passed parameter name. (parameter: {parameter_name})'
+        for line in fileinput.input(settings_file, inplace=True):
+            if re.match(line_pattern, line):
+                parameter_is_exist = True
+                line = re.sub(line_pattern, new_line, line)
+            sys.stdout.write(line)
 
-    new_line = f'{parameter_name} = {value}'
-    line_pattern = fr'^{parameter_name} = .*'
-
-    for line in fileinput.input(settings_file, inplace=True):
-        if re.match(line_pattern, line):
-            parameter_is_exist = True
-            line = re.sub(line_pattern, new_line, line)
-
-        sys.stdout.write(line)
-
-    if not parameter_is_exist:
-        return False, f'Can\'t find passed parameter name. (parameter: {parameter_name})'
-
-    return True, f'{parameter_name} updated successfully.'
+        if not parameter_is_exist:
+            raise NameError(f"Can't find parameter name: {parameter_name}")
+        return True
+    return False
--- testproject/tests.py
+++ testproject/tests.py
@@ -1,7 +1,5 @@
-import os
 from io import StringIO
 
-from django.conf import settings
 from django.core.management import call_command
 from django.test import TestCase
 
@@ -9,32 +7,27 @@ from djecrety.utils import generate_secret_key, save_to_settings
 
 
 class GenerateSecretKeyTests(TestCase):
+
     def test_with_none_length(self):
         """
         Check secret key with none type length has been generated or not.
         """
         secret_key_length = None
-        secret_key = generate_secret_key(secret_key_length)
-
-        self.assertEqual(len(secret_key), 50)
+        self.assertRaises(TypeError, generate_secret_key, secret_key_length)
 
     def test_with_none_integer_length(self):
         """
         Check secret key with string type length has been generated or not.
         """
         secret_key_length = 'test'
-        secret_key = generate_secret_key(secret_key_length)
-
-        self.assertEqual(len(secret_key), 50)
+        self.assertRaises(TypeError, generate_secret_key, secret_key_length)
 
     def test_with_none_integer_empty_string_length(self):
         """
         Check secret key with empty string length has been generated or not.
         """
         secret_key_length = ''
-        secret_key = generate_secret_key(secret_key_length)
-
-        self.assertEqual(len(secret_key), 50)
+        self.assertRaises(TypeError, generate_secret_key, secret_key_length)
 
     def test_with_zero_length(self):
         """
@@ -42,7 +35,6 @@ class GenerateSecretKeyTests(TestCase):
         """
         secret_key_length = 0
         secret_key = generate_secret_key(secret_key_length)
-
         self.assertEqual(len(secret_key), secret_key_length)
 
     def test_with_passed_integer_length(self):
@@ -51,7 +43,6 @@ class GenerateSecretKeyTests(TestCase):
         """
         secret_key_length = 20
         secret_key = generate_secret_key(secret_key_length)
-
         self.assertEqual(len(secret_key), secret_key_length)
 
     def test_without_passed_length(self):
@@ -60,35 +51,32 @@ class GenerateSecretKeyTests(TestCase):
         """
         default_length = 50
         secret_key = generate_secret_key()
-
         self.assertEqual(len(secret_key), default_length)
 
 
 class SaveToSettingsTests(TestCase):
+
     def test_with_parameter_value_with_defaults(self):
         """
         Check value has been updated or not.
         """
-        parameter_value = f'\'{generate_secret_key()}\''
+        parameter_value = f"'{generate_secret_key()}'"
         parameter_name = 'SECRET_KEY'
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(parameter_value, parameter_name, settings_dir_name)
-
+        result = save_to_settings(parameter_value, parameter_name,
+                                  settings_dir_name)
         self.assertEqual(result, True)
-        self.assertEqual(message, f'{parameter_name} updated successfully.')
 
     def test_with_empty_parameter_value(self):
         """
         Check empty value has been updated or not.
         """
-        parameter_value = '\'\''
+        parameter_value = "''"
         parameter_name = 'SECRET_KEY'
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(parameter_value, parameter_name, settings_dir_name)
-
+        result = save_to_settings(parameter_value, parameter_name,
+                                  settings_dir_name)
         self.assertEqual(result, True)
-        self.assertEqual(message, f'{parameter_name} updated successfully.')
-
         self.test_with_parameter_value_with_defaults()
 
     def test_with_none_parameter_value(self):
@@ -98,11 +86,9 @@ class SaveToSettingsTests(TestCase):
         parameter_value = None
         parameter_name = 'SECRET_KEY'
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(parameter_value, parameter_name, settings_dir_name)
-
+        result = save_to_settings(parameter_value, parameter_name,
+                                  settings_dir_name)
         self.assertEqual(result, True)
-        self.assertEqual(message, f'{parameter_name} updated successfully.')
-
         self.test_with_parameter_value_with_defaults()
 
     def test_with_correct_parameter_name(self):
@@ -111,10 +97,9 @@ class SaveToSettingsTests(TestCase):
         """
         parameter_name = 'DEBUG'
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(True, parameter_name, settings_dir_name)
-
+        result = save_to_settings(True, parameter_name,
+                                  settings_dir_name)
         self.assertEqual(result, True)
-        self.assertEqual(message, f'{parameter_name} updated successfully.')
 
     def test_with_incorrect_parameter_name(self):
         """
@@ -122,10 +107,8 @@ class SaveToSettingsTests(TestCase):
         """
         parameter_name = 'TEST'
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(False, parameter_name, settings_dir_name)
-
-        self.assertEqual(result, False)
-        self.assertEqual(message, f'Can\'t find passed parameter name. (parameter: {parameter_name})')
+        self.assertRaises(NameError, save_to_settings, False, parameter_name,
+                          settings_dir_name)
 
     def test_with_empty_parameter_name(self):
         """
@@ -133,10 +116,8 @@ class SaveToSettingsTests(TestCase):
         """
         parameter_name = ''
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(False, parameter_name, settings_dir_name)
-
+        result = save_to_settings(False, parameter_name, settings_dir_name)
         self.assertEqual(result, False)
-        self.assertEqual(message, f'Can\'t find passed parameter name. (parameter: {parameter_name})')
 
     def test_with_None_parameter_name(self):
         """
@@ -144,98 +125,90 @@ class SaveToSettingsTests(TestCase):
         """
         parameter_name = None
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(False, parameter_name, settings_dir_name)
-
+        result = save_to_settings(False, parameter_name, settings_dir_name)
         self.assertEqual(result, False)
-        self.assertEqual(message, f'Can\'t find passed parameter name. (parameter: {parameter_name})')
 
     def test_with_incorrect_settings_dir_name(self):
         """
         Check passed settings dir name is exist or not.
         """
-        parameter_value = f'\'{generate_secret_key()}\''
+        parameter_value = f"'{generate_secret_key()}'"
         parameter_name = 'SECRET_KEY'
         settings_dir_name = 'test'
-        settings_dir = os.path.join(settings.BASE_DIR, settings_dir_name, 'settings.py')
-        result, message = save_to_settings(parameter_value, parameter_name, settings_dir_name='test')
-
-        self.assertEqual(result, False)
-        self.assertEqual(message,
-                         f'Not found: {settings_dir}\n\nCan\'t find \'settings.py\' file,'
-                         ' please specify settings file directory name using -p argument.')
+        self.assertRaises(FileNotFoundError, save_to_settings, parameter_value,
+                          parameter_name, settings_dir_name)
 
     def test_with_correct_settings_dir_name(self):
         """
         Check passed settings dir name is exist or not.
         """
-        parameter_value = f'\'{generate_secret_key()}\''
+        parameter_value = f"'{generate_secret_key()}'"
         parameter_name = 'SECRET_KEY'
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(parameter_value, parameter_name, settings_dir_name=settings_dir_name)
-
+        result = save_to_settings(parameter_value, parameter_name,
+                                  settings_dir_name)
         self.assertEqual(result, True)
-        self.assertEqual(message, f'{parameter_name} updated successfully.')
 
     def test_with_empty_settings_dir_name(self):
         """
         Check passed settings dir name is empty or not.
         """
-        parameter_value = f'\'{generate_secret_key()}\''
+        parameter_value = f"'{generate_secret_key()}'"
         parameter_name = 'SECRET_KEY'
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(parameter_value, parameter_name, settings_dir_name=settings_dir_name)
-
+        result = save_to_settings(parameter_value, parameter_name,
+                                  settings_dir_name)
         self.assertEqual(result, True)
-        self.assertEqual(message, f'{parameter_name} updated successfully.')
 
     def test_with_none_settings_dir_name(self):
         """
         Check passed settings dir name is empty or not.
         """
-        parameter_value = f'\'{generate_secret_key()}\''
+        parameter_value = f"'{generate_secret_key()}'"
         parameter_name = 'SECRET_KEY'
         settings_dir_name = 'testproject'
-        result, message = save_to_settings(parameter_value, parameter_name, settings_dir_name=settings_dir_name)
-
+        result = save_to_settings(parameter_value, parameter_name,
+                                  settings_dir_name)
         self.assertEqual(result, True)
-        self.assertEqual(message, f'{parameter_name} updated successfully.')
 
 
 class CommandsTestCase(TestCase):
+
     def test_djecrety(self):
         """
         Test djecrety command.
         """
+        parameter_name = 'SECRET_KEY'
         out = StringIO()
         args = []
         opts = {}
         call_command('djecrety', stdout=out, *args, **opts)
-
-        self.assertTrue('New secret key:' in out.getvalue())
+        self.assertTrue(parameter_name in out.getvalue())
 
     def test_djecrety_with_save_arg(self):
         """
         Test djecrety command with save argument.
         """
+        parameter_name = 'SECRET_KEY'
         out = StringIO()
         args = ['-s']
         opts = {'settings_dir_name': 'testproject'}
         call_command('djecrety', stdout=out, *args, **opts)
-
-        self.assertTrue('SECRET_KEY updated successfully.' in out.getvalue())
+        self.assertTrue(f'{parameter_name} updated successfully.' in
+                        out.getvalue())
 
     def test_djecrety_with_save_and_display_arg(self):
         """
         Test djecrety command with save and display arguments together.
         """
+        parameter_name = 'SECRET_KEY'
         out = StringIO()
         args = ['-sp']
         opts = {'settings_dir_name': 'testproject'}
         call_command('djecrety', stdout=out, *args, **opts)
+        self.assertTrue(f'{parameter_name} set to:' in out.getvalue())
 
-        self.assertTrue('SECRET_KEY set to:' in out.getvalue())
-
-    def test_djecrety_with_settings_dir_arg(self):
+    def test_djecrety_with_incorrect_settings_dir_arg(self):
         """
         Test djecrety command with specify settings directory name argument.
         """
@@ -243,5 +216,24 @@ class CommandsTestCase(TestCase):
         args = ['-sp']
         opts = {'settings_dir_name': 'test'}
         call_command('djecrety', stdout=out, *args, **opts)
+        self.assertTrue("Can't find `settings.py` file: " in out.getvalue())
 
-        self.assertTrue('Not found:' in out.getvalue())
+    def test_djecrety_with_none_settings_dir_arg(self):
+        """
+        Test djecrety command with specify settings directory name argument.
+        """
+        out = StringIO()
+        args = ['-sp']
+        opts = {'settings_dir_name': None}
+        call_command('djecrety', stdout=out, *args, **opts)
+        self.assertTrue("Can't find `settings.py` file: " in out.getvalue())
+
+    def test_djecrety_with_empty_settings_dir_arg(self):
+        """
+        Test djecrety command with specify settings directory name argument.
+        """
+        out = StringIO()
+        args = ['-sp']
+        opts = {'settings_dir_name': ''}
+        call_command('djecrety', stdout=out, *args, **opts)
+        self.assertTrue("Can't find `settings.py` file: " in out.getvalue())
