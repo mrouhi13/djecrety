4d6a8e10744a9fb0003a2475f7cdd23bae508e4f
mrouhi13 <mrouhi13@gmail.com>
2021-03-27 12:04:32 +0430

    remove unused comments

--- testproject/settings.py
+++ testproject/settings.py
@@ -1,29 +1,17 @@
 import os
 import sys
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Add djecrety package to PYTHON_PATH
 sys.path.insert(0, BASE_DIR)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '_'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-# Application definition
 
 INSTALLED_APPS = [
     'djecrety',
 ]
-
-# Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
     'default': {
